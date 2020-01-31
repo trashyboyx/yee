2c8d6d1f4fa134a5ea86167664eb96724c64134d
trashyboyx <martinsmarcos6@icloud.com>
2020-01-31 08:39:24 -0300

    yee

--- test/Program.cs
+++ test/Program.cs
@@ -7,6 +7,9 @@ namespace test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("yee");
+
+            Console.ReadLine();
         }
     }
 }
